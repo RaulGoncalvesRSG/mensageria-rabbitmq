4c4d5fd4f1fb6745ebc91494be676b287f3b0058
Raul Gonçalves <raulsantos121@gmail.com>
2022-10-20 09:00:40 -0300

    Consumidor de String

--- consumer-service/src/main/java/com/raul/consumer/consumers/rabbitmq/consumers/StringConsumer.java
+++ consumer-service/src/main/java/com/raul/consumer/consumers/rabbitmq/consumers/StringConsumer.java
@@ -1,4 +1,15 @@
 package com.raul.consumer.consumers.rabbitmq.consumers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class StringConsumer {
+
+    @RabbitListener(queues = "product.log")
+    public void consumer(String message){
+        log.info("Mensagem consumida: " + message);
+    }
 }
